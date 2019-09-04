6bb066d7d340c0bf7b2d91011f7f1a23262653f9
yushulx <lingxiao1002@gmail.com>
2019-09-04 15:36:31 +0800

    Updated search configuration

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from google_images_download import google_images_download
 import sys
 import cv2
 import os
@@ -6,8 +7,6 @@ from dbr import DynamsoftBarcodeReader
 dbr = DynamsoftBarcodeReader()
 dbr.initLicense('LICENSE-KEY')
 
-
-from google_images_download import google_images_download  
 
 def decodeFile(fileName, templateName=""):
     # 1D, PDF417, QRCODE, DataMatrix, Aztec Code
@@ -18,23 +17,27 @@ def decodeFile(fileName, templateName=""):
         print("Format: " + result[0])
         print("Value: " + result[1])
 
+
 def read_folder(folder):
     for f in os.listdir(folder):
         print("-------------------------------------------------\n")
+        print(f)
         decodeFile(os.path.join(folder, f))
 
+
 if __name__ == "__main__":
-    response = google_images_download.googleimagesdownload()  
 
     keyword = "barcode on box"
+    response = google_images_download.googleimagesdownload()  
 
-    arguments = {"keywords": keyword, "limit": 10, "format": "jpg"}  
+    arguments = {"keywords": keyword, "limit": 10, "format": "jpg", "usage_rights": "labeled-for-reuse", "time_range": '{"time_min": "01/01/2010", "time_max": "01/01/2019"}'}  
     paths = response.download(arguments) 
 
     images = paths[0][keyword]
 
     for filename in images:
-        print("-------------------------------------------------\n")
-        decodeFile(filename)
-        
+            print("-------------------------------------------------\n")
+            print(filename)
+            decodeFile(filename)
+            
     # read_folder(os.path.join(os.getcwd(), 'downloads', keyword))
